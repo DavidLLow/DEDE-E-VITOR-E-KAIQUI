841cebc271a443bde8d7f98244e919734d93b8f9
DEDE <davidlodigiani16@gmail.com>
2026-05-21 15:46:52 -0300

    PRONTO ESTA NO AR

---
+++ produtos.js
@@ -0,0 +1,31 @@
+const searchInput =
+document.getElementById('searchInput');
+
+searchInput.addEventListener('keyup', ()=>{
+
+  const filter =
+  searchInput.value.toLowerCase();
+
+  const cards =
+  document.querySelectorAll('.product-card');
+
+  cards.forEach(card=>{
+
+    const title =
+    card.querySelector('h3')
+    .textContent
+    .toLowerCase();
+
+    if(title.includes(filter)){
+
+      card.style.display = 'block';
+
+    }else{
+
+      card.style.display = 'none';
+
+    }
+
+  });
+
+});
